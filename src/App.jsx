910bcdeca19319d3c18638b090171e86b6fa981c
Saksham Singh Yadav <sakshamsinghyadav@Sakshams-MacBook-Air-3.local>
2026-07-22 22:28:32 +0530

    Day 2: Created React project and homepage

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>AI Medical Report Explainer</h1>
+
+      <p>
+        Understand your medical reports in simple language.
+      </p>
+    </div>
+  );
+}
+
+export default App;
