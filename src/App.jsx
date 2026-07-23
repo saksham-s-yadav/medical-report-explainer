952b43d410e76c0408d8b030cd75f3d24f0890a9
saksham-s-yadav <singhyadavsaksham@gmail.com>
2026-07-23 11:48:47 +0530

    Day 3: Improved hero section and fixed heading

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,43 @@ import "./App.css";
 function App() {
   return (
     <div className="app">
-      <h1>AI Medical Report Explainer</h1>
 
-      <p>
-        Understand your medical reports in simple language.
-      </p>
+      <nav className="navbar">
+        <h2>🩺 MedExplain AI</h2>
+
+        <div className="nav-links">
+          <a href="#">Home</a>
+          <a href="#">Features</a>
+          <a href="#">About</a>
+          <a href="#">Contact</a>
+        </div>
+      </nav>
+
+      <section className="hero">
+
+        <h1>
+          AI Medical Report
+          <br />
+          Explainer
+        </h1>
+
+        <p>
+          Understand complex medical reports in simple,
+          easy-to-read language powered by Artificial Intelligence.
+        </p>
+
+        <div className="buttons">
+          <button className="primary">
+            Upload Report
+          </button>
+
+          <button className="secondary">
+            Learn More
+          </button>
+        </div>
+
+      </section>
+
     </div>
   );
 }
